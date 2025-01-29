72cdb124c88600fe6ed1b935168ebe0c43a5f3d2
Norm Johanson <normj@amazon.com>
2025-01-29 09:13:17 -0800

    Add initial Lambda development integration (#18)

---
+++ src/Aspire.Hosting.AWS/Lambda/Enumerations.cs
@@ -0,0 +1,13 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+namespace Aspire.Hosting.AWS.Lambda;
+
+/// <summary>
+/// The type of of API Gateway to configure the emulator for.
+/// </summary>
+public enum APIGatewayType { Rest, HttpV1, HttpV2 }
+
+/// <summary>
+/// The HTTP method a Lambda function should be called for.
+/// </summary>
+public enum Method { Any, Get, Post, Put, Delete, Patch, Head, Options}
---
+++ src/Aspire.Hosting.AWS/Lambda/LambdaProjectResource.cs
@@ -0,0 +1,13 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+using Aspire.Hosting.ApplicationModel;
+
+namespace Aspire.Hosting.AWS.Lambda;
+
+/// <summary>
+/// Aspire resource representing a Lambda function.
+/// </summary>
+/// <param name="name"></param>
+public class LambdaProjectResource(string name) : ProjectResource(name)
+{
+}
