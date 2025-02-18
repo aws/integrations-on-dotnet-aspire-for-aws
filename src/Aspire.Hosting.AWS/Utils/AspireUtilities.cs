10d219ea5480fbb748da5f03789ccf33f4faed64
Norm Johanson <normj@amazon.com>
2025-02-18 12:00:52 -0800

    Add check if running in the debugger when working with Lambda class libraries.

---
+++ src/Aspire.Hosting.AWS/Utils/AspireUtilities.cs
@@ -0,0 +1,8 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+namespace Aspire.Hosting.AWS.Utils;
+
+public static class AspireUtilities
+{
+    public static bool IsRunningInDebugger => !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DEBUG_SESSION_PORT"));
+}
