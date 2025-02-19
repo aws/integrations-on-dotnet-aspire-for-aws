bb1c302d425053efa2a38132f2edcd46047a5622
Phil Asmar <phil.asmar@gmail.com>
2025-02-19 09:15:33 -0500

    reply to review comments

--- playground/Lambda/WebAWSCallsLambdaFunction/Program.cs
+++ playground/Lambda/WebAWSCallsLambdaFunction/Program.cs
@@ -7,6 +7,7 @@ var builder = new HostApplicationBuilder();
 builder.AddServiceDefaults();
 builder.Services.AddAWSService<Amazon.DynamoDBv2.IAmazonDynamoDB>();
 builder.Services.AddAWSService<Amazon.SQS.IAmazonSQS>();
+builder.Services.AddAWSService<Amazon.SecurityToken.IAmazonSecurityTokenService>();
 builder.Services.AddHostedService<LambdaFunction>();
 
 builder.Build().Run();
