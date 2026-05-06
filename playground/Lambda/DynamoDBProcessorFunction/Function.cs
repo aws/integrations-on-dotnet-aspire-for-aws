54eb5b5c83759029fdcadcc45d8972cb06e37e9f
Norm Johanson <normj@amazon.com>
2026-05-06 10:21:56 -0700

    Add DynamoDB streaming example to playground

---
+++ playground/Lambda/DynamoDBProcessorFunction/Function.cs
@@ -0,0 +1,46 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.DynamoDBEvents;
+using Amazon.Lambda.SQSEvents;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Instrumentation.AWSLambda;
+using OpenTelemetry.Trace;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace DynamoDBProcessorFunction;
+
+public class Function
+{
+    IHost _host;
+    TracerProvider _traceProvider;
+
+    public Function()
+    {
+        var builder = new HostApplicationBuilder();
+
+        builder.AddServiceDefaults();
+        _host = builder.Build();
+
+        _traceProvider = _host.Services.GetRequiredService<TracerProvider>();
+    }
+
+    public Task FunctionHandler(DynamoDBEvent dynamoEvent, ILambdaContext context)
+        => AWSLambdaWrapper.TraceAsync(_traceProvider, async (dynamoEvent, context) =>
+    {
+        context.Logger.LogInformation($"Beginning to process {dynamoEvent.Records.Count} records...");
+
+        foreach (var record in dynamoEvent.Records)
+        {
+            context.Logger.LogInformation($"Event ID: {record.EventID}");
+            context.Logger.LogInformation($"Event Name: {record.EventName}");
+
+            // TODO: Add business logic processing the record.Dynamodb object.
+        }
+
+        context.Logger.LogInformation("Stream processing complete.");
+
+        return Task.CompletedTask;
+    }, dynamoEvent, context);
+}
--- playground/Lambda/Lambda.AppHost/Program.cs
+++ playground/Lambda/Lambda.AppHost/Program.cs
@@ -1,8 +1,14 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+using Amazon.CDK.AWS.DynamoDB;
 using Amazon.Lambda;
 using Aspire.Hosting.AWS.Lambda;
 
 var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddAWSLambdaServiceEmulator(new LambdaEmulatorOptions
+{
+    DisableAutoInstall = true
+});
 
 var awsSdkConfig = builder.AddAWSSDKConfig().WithRegion(Amazon.RegionEndpoint.USWest2);
 
@@ -11,6 +17,13 @@ cdkStackResource.WithTag("aws-repo", "integrations-on-dotnet-aspire-for-aws");
 
 var sqsDemoQueue1 = cdkStackResource.AddSQSQueue("DemoQueue1");
 var sqsDemoQueue2 = cdkStackResource.AddSQSQueue("DemoQueue2");
+
+var table = cdkStackResource.AddDynamoDBTable("DemoTable", new TableProps
+{
+    PartitionKey = new Amazon.CDK.AWS.DynamoDB.Attribute { Name = "Id", Type = AttributeType.STRING },
+    Stream = StreamViewType.NEW_AND_OLD_IMAGES,
+    BillingMode = BillingMode.PAY_PER_REQUEST
+});
 
 builder.AddAWSLambdaFunction<Projects.ToUpperLambdaFunctionExecutable>("ToUpperFunction", lambdaHandler: "ToUpperLambdaFunctionExecutable", new LambdaFunctionOptions { ApplicationLogLevel = ApplicationLogLevel.DEBUG, LogFormat = LogFormat.JSON });
 
@@ -40,6 +53,14 @@ builder.AddAWSLambdaFunction<Projects.SQSProcessorFunction>("SQSProcessorFunctio
         // CDK output parameters are not attempted to be added.
         .WithReference(sqsDemoQueue1)
         .WithReference(sqsDemoQueue2);
+
+
+builder.AddAWSLambdaFunction<Projects.DynamoDBProcessorFunction>("DynamoDBProcessorFunction", "DynamoDBProcessorFunction::DynamoDBProcessorFunction.Function::FunctionHandler")
+        .WithDynamoDBStreamsEventSource(table, new DynamoDBStreamsEventSourceOptions
+        {
+            BatchSize = 5
+        })
+        .WithDynamoDBStreamsEventSource("StreamingTest");
 
 
 builder.Build().Run();
